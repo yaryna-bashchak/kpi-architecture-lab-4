419b5996910633667cf844a275774497f361d383
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2023-06-02 18:01:21 +0300

    add unit tests to check health

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/jarcoal/httpmock"
 	. "gopkg.in/check.v1"
 	
 	"testing"
@@ -69,4 +73,39 @@ func (s *MySuite) TestFindMinServer(c *C) {
 		{URL: "Server3", ConnCnt: 17, Healthy: false},
 	}
 	c.Assert(FindMinServer(), Equals, -1)
+
+func (s *MySuite) TestServerHealth_Healthy(c *C) {
+	mockURL := "http://example.com/Health"
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	server := &Server{
+		URL: "example.com",
+	}
+
+	httpmock.RegisterResponder(http.MethodGet, mockURL, httpmock.NewStringResponder(http.StatusOK, ""))
+	
+	healthyCheckResult := Health(server)
+	c.Check(healthyCheckResult, Equals, true)
+	c.Check(server.Healthy, Equals, true)
+}
+
+func (s *MySuite) TestServerHealth_Unhealthy(c *C) {
+	mockURL := "http://example.com/Health"
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	server := &Server{
+		URL: "example.com",
+		Healthy: false,
+	}
+
+	httpmock.RegisterResponder(http.MethodGet, mockURL, httpmock.NewStringResponder(http.StatusInternalServerError, ""))
+
+	unhealthyCheckResult := Health(server)
+	c.Check(unhealthyCheckResult, Equals, false)
+	c.Check(server.Healthy, Equals, false)
+}
 }
