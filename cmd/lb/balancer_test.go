6e69c76ac9396df0eeeabe850ad0b4f32bf089b9
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2023-06-02 17:59:53 +0300

    add unit test for FindMinserver

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -31,3 +31,42 @@ func (s *MySuite) TestScheme(c *C) {
 		c.Check(scheme(), Equals, tc.expected)
 	}
 }
+
+func (s *MySuite) TestFindMinServer(c *C) {
+	c.Assert(FindMinServer(), Equals, -1)
+
+	serversPool = []*Server{
+		{URL: "Server1", ConnCnt: 22, Healthy: true},
+		{URL: "Server2", ConnCnt: 17, Healthy: true},
+		{URL: "Server3", ConnCnt: 35, Healthy: true},
+	}
+	c.Assert(FindMinServer(), Equals, 1)
+	
+	serversPool = []*Server{
+		{URL: "Server1", ConnCnt: 10, Healthy: true},
+		{URL: "Server2", ConnCnt: 10, Healthy: true},
+		{URL: "Server3", ConnCnt: 10, Healthy: true},
+	}
+	c.Assert(FindMinServer(), Equals, 0)
+
+	serversPool = []*Server{
+		{URL: "Server1", ConnCnt: 1, Healthy: false},
+		{URL: "Server2", ConnCnt: 15, Healthy: true},
+		{URL: "Server3", ConnCnt: 12, Healthy: true},
+	}
+	c.Assert(FindMinServer(), Equals, 2)
+
+	serversPool = []*Server{
+		{URL: "Server1", ConnCnt: 0, Healthy: false},
+		{URL: "Server2", ConnCnt: 10, Healthy: true},
+		{URL: "Server3", ConnCnt: 0, Healthy: false},
+	}
+	c.Assert(FindMinServer(), Equals, 1)
+
+	serversPool = []*Server{
+		{URL: "Server1", ConnCnt: 15, Healthy: false},
+		{URL: "Server2", ConnCnt: 12, Healthy: false},
+		{URL: "Server3", ConnCnt: 17, Healthy: false},
+	}
+	c.Assert(FindMinServer(), Equals, -1)
+}
