9b97297b3eb1f8aa3a1912c7cb7dcd590a441767
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2023-06-02 17:59:06 +0300

    add unit test for sheme

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -1,7 +1,33 @@
 package main
 
-import "testing"
+import (
+	. "gopkg.in/check.v1"
+	
+	"testing"
+)
+
+type MySuite struct{}
+
+var _ = Suite(&MySuite{})
 
 func TestBalancer(t *testing.T) {
-	// TODO: Реалізуйте юніт-тест для балансувальникка.
+	TestingT(t)
+}
+
+func (s *MySuite) TestScheme(c *C) {
+	testCases := []struct {
+		name     string
+		https    bool
+		expected string
+	}{
+		{"HTTP", false, "http"},
+		{"HTTPS", true, "https"},
+		{"Reset to HTTP", false, "http"},
+	}
+
+	for _, tc := range testCases {
+		c.Log(tc.name)
+		*https = tc.https
+		c.Check(scheme(), Equals, tc.expected)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/yaryna-bashchak/kpi-architecture-lab-4
 
 go 1.20
+
+require (
+	github.com/jarcoal/httpmock v1.3.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
