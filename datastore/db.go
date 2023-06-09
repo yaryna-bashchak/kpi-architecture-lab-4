97a74ffc2051eb471c1e762fb5dc8cc1f0beff34
Nikolay <90026719+TsNikolay@users.noreply.github.com>
2023-06-09 13:02:11 +0200

    Adding segments

--- datastore/db.go
+++ datastore/db.go
@@ -8,32 +8,34 @@ import (
 	"os"
 	"path/filepath"
 )
-
 const outFileName = "current-data"
-
 var ErrNotFound = fmt.Errorf("record does not exist")
-
 type hashIndex map[string]int64
 
 type Db struct {
-	out *os.File
-	outPath string
-	outOffset int64
+	out              *os.File
+	outPath          string
+	outOffset        int64
+	dir              string
+	segmentSize      int64
+	lastSegmentIndex int
 
-	index hashIndex
+	index    hashIndex
+	segments []*Segment
 }
 
-func NewDb(dir string) (*Db, error) {
-	outputPath := filepath.Join(dir, outFileName)
-	f, err := os.OpenFile(outputPath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0o600)
+func NewDb(dir string, segmentSize int64) (*Db, error) {
+	db := &Db{
+		segments:    make([]*Segment, 0),
+		dir:         dir,
+		segmentSize: segmentSize,
+	}
+
+	err := db.createSegment()
 	if err != nil {
 		return nil, err
 	}
-	db := &Db{
-		outPath: outputPath,
-		out:     f,
-		index:   make(hashIndex),
-	}
+
 	err = db.recover()
 	if err != nil && err != io.EOF {
 		return nil, err
@@ -43,19 +45,88 @@ func NewDb(dir string) (*Db, error) {
 
 const bufSize = 8192
 
-func (db *Db) recover() error {
-	input, err := os.Open(db.outPath)
+func (db *Db) createSegment() error {
+	filePath := db.getNewFileName()
+	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0777)
 	if err != nil {
 		return err
 	}
-	defer input.Close()
 
+	newSegment := &Segment{
+		filePath: filePath,
+		index:    make(hashIndex),
+	}
+	db.out = f
+	db.outOffset = 0
+	db.segments = append(db.segments, newSegment)
+	if len(db.segments) >= 3 {
+		db.compactOldSegments()
+	}
+	return err
+}
+
+func (db *Db) getNewFileName() string {
+	result := filepath.Join(db.dir, fmt.Sprintf("%s%d", outFileName, db.lastSegmentIndex))
+	db.lastSegmentIndex++
+	return result
+}
+
+func (db *Db) compactOldSegments() {
+	go func() {
+		filePath := db.getNewFileName()
+		newSegment := &Segment{
+			filePath: filePath,
+			index:    make(hashIndex),
+		}
+		var offset int64
+		f, err := os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0o600)
+		if err != nil {
+			return
+		}
+		lastSegmentIndex := len(db.segments) - 2
+		for i := 0; i <= lastSegmentIndex; i++ {
+			s := db.segments[i]
+			for key, index := range s.index {
+				if i < lastSegmentIndex {
+					isInNewerSegments := checkKeyInSegments(db.segments[i+1:lastSegmentIndex+1], key)
+					if isInNewerSegments {
+						continue
+					}
+				}
+				value, _ := s.getFromSegment(index)
+				e := entry{
+					key:   key,
+					value: value,
+				}
+				n, err := f.Write(e.Encode())
+				if err == nil {
+					newSegment.index[key] = offset
+					offset += int64(n)
+				}
+			}
+		}
+		db.segments = []*Segment{newSegment, db.getLastSegment()}
+	}()
+}
+
+func checkKeyInSegments(segments []*Segment, key string) bool {
+	for _, s := range segments {
+		if _, ok := s.index[key]; ok {
+			return true
+		}
+	}
+	return false
+}
+
+func (db *Db) recover() error {
+	var err error
 	var buf [bufSize]byte
-	in := bufio.NewReaderSize(input, bufSize)
+
+	in := bufio.NewReaderSize(db.out, bufSize)
 	for err == nil {
 		var (
 			header, data []byte
-			n int
+			n            int
 		)
 		header, err = in.Peek(bufSize)
 		if err == io.EOF {
@@ -66,14 +137,12 @@ func (db *Db) recover() error {
 			return err
 		}
 		size := binary.LittleEndian.Uint32(header)
-
 		if size < bufSize {
 			data = buf[:size]
 		} else {
 			data = make([]byte, size)
 		}
 		n, err = in.Read(data)
-
 		if err == nil {
 			if n != int(size) {
 				return fmt.Errorf("corrupted file")
@@ -81,40 +150,42 @@ func (db *Db) recover() error {
 
 			var e entry
 			e.Decode(data)
-			db.index[e.key] = db.outOffset
-			db.outOffset += int64(n)
+			db.setKey(e.key, int64(n))
 		}
 	}
 	return err
 }
-
 func (db *Db) Close() error {
 	return db.out.Close()
 }
 
+func (db *Db) setKey(key string, n int64) {
+	db.getLastSegment().index[key] = db.outOffset
+	db.outOffset += n
+}
+
+func (db *Db) getSegmentAndPosition(key string) (*Segment, int64, error) {
+	for i := range db.segments {
+		s := db.segments[len(db.segments)-i-1]
+		pos, ok := s.index[key]
+		if ok {
+			return s, pos, nil
+		}
+	}
+
+	return nil, 0, ErrNotFound
+}
+
 func (db *Db) Get(key string) (string, error) {
-	position, ok := db.index[key]
-	if !ok {
-		return "", ErrNotFound
-	}
-
-	file, err := os.Open(db.outPath)
+	segment, position, err := db.getSegmentAndPosition(key)
 	if err != nil {
 		return "", err
 	}
-	defer file.Close()
+	return segment.getFromSegment(position)
+}
 
-	_, err = file.Seek(position, 0)
-	if err != nil {
-		return "", err
-	}
-
-	reader := bufio.NewReader(file)
-	value, err := readValue(reader)
-	if err != nil {
-		return "", err
-	}
-	return value, nil
+func (db *Db) getLastSegment() *Segment {
+	return db.segments[len(db.segments)-1]
 }
 
 func (db *Db) Put(key, value string) error {
@@ -122,10 +193,45 @@ func (db *Db) Put(key, value string) error {
 		key:   key,
 		value: value,
 	}
+	length := e.getLength()
+	stat, err := db.out.Stat()
+	if err != nil {
+		return err
+	}
+	if stat.Size()+length > db.segmentSize {
+		err := db.createSegment()
+		if err != nil {
+			return err
+		}
+	}
 	n, err := db.out.Write(e.Encode())
 	if err == nil {
-		db.index[key] = db.outOffset
-		db.outOffset += int64(n)
+		db.setKey(e.key, int64(n))
 	}
 	return err
+}
+
+type Segment struct {
+	outOffset int64
+
+	index    hashIndex
+	filePath string
+}
+
+func (s *Segment) getFromSegment(position int64) (string, error) {
+	file, err := os.Open(s.filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+	_, err = file.Seek(position, 0)
+	if err != nil {
+		return "", err
+	}
+	reader := bufio.NewReader(file)
+	value, err := readValue(reader)
+	if err != nil {
+		return "", err
+	}
+	return value, nil
 }
--- datastore/entry.go
+++ datastore/entry.go
@@ -7,7 +7,12 @@ import (
 )
 
 type entry struct {
-	key, value string
+	key   string
+	value string
+}
+
+func (e *entry) getLength() int64 {
+	return int64(len(e.key) + len(e.value) + 12)
 }
 
 func (e *entry) Encode() []byte {
@@ -15,24 +20,19 @@ func (e *entry) Encode() []byte {
 	vl := len(e.value)
 	size := kl + vl + 12
 	res := make([]byte, size)
-	binary.LittleEndian.PutUint32(res, uint32(size))
-	binary.LittleEndian.PutUint32(res[4:], uint32(kl))
-	copy(res[8:], e.key)
-	binary.LittleEndian.PutUint32(res[kl+8:], uint32(vl))
-	copy(res[kl+12:], e.value)
+	binary.LittleEndian.PutUint32(res[:4], uint32(size))
+	binary.LittleEndian.PutUint32(res[4:8], uint32(kl))
+	copy(res[8:8+kl], e.key)
+	binary.LittleEndian.PutUint32(res[8+kl:12+kl], uint32(vl))
+	copy(res[12+kl:], e.value)
 	return res
 }
 
 func (e *entry) Decode(input []byte) {
-	kl := binary.LittleEndian.Uint32(input[4:])
-	keyBuf := make([]byte, kl)
-	copy(keyBuf, input[8:kl+8])
-	e.key = string(keyBuf)
-
-	vl := binary.LittleEndian.Uint32(input[kl+8:])
-	valBuf := make([]byte, vl)
-	copy(valBuf, input[kl+12:kl+12+vl])
-	e.value = string(valBuf)
+	kl := binary.LittleEndian.Uint32(input[4:8])
+	e.key = string(input[8 : 8+kl])
+	vl := binary.LittleEndian.Uint32(input[8+kl : 12+kl])
+	e.value = string(input[12+kl : 12+kl+vl])
 }
 
 func readValue(in *bufio.Reader) (string, error) {
@@ -40,12 +40,11 @@ func readValue(in *bufio.Reader) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	keySize := int(binary.LittleEndian.Uint32(header[4:]))
+	keySize := int(binary.LittleEndian.Uint32(header[4:8]))
 	_, err = in.Discard(keySize + 8)
 	if err != nil {
 		return "", err
 	}
-
 	header, err = in.Peek(4)
 	if err != nil {
 		return "", err
@@ -55,7 +54,6 @@ func readValue(in *bufio.Reader) (string, error) {
 	if err != nil {
 		return "", err
 	}
-
 	data := make([]byte, valSize)
 	n, err := in.Read(data)
 	if err != nil {
@@ -64,6 +62,5 @@ func readValue(in *bufio.Reader) (string, error) {
 	if n != valSize {
 		return "", fmt.Errorf("can't read value bytes (read %d, expected %d)", n, valSize)
 	}
-
 	return string(data), nil
 }
